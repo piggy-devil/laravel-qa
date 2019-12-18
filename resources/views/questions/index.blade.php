c93909d5768f19ac211f49d4b864c373e13f3c73
muupa <muupa@supatcharakuns-MacBook-Pro.local>
2019-12-18 15:26:33 +0700

    Add author info and question creation date in question item

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -11,7 +11,12 @@
                    @foreach ($questions as $question)
                         <div class="media">
                             <div class="media-body">
-                                <h3 class="mt-0">{{ $question->title }}</h3>
+                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</h3>
+                                <p class="lead">
+                                    Asked body
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                    <small class="text-muted">{{ $question->created_date }}</small>
+                                </p>
                                 {{ Str::limit($question->body, 250) }}
                             </div>
                         </div>
