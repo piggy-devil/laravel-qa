8e65791f64422325e7d9605435372ba106dfd4e1
muupa <muupa@supatcharakuns-MacBook-Pro.local>
2020-01-03 15:20:26 +0700

    Securing the application from xss attack

--- app/Question.php
+++ app/Question.php
@@ -47,7 +47,7 @@ class Question extends Model
 
     public function getBodyHtmlAttribute()
     {
-        return \Parsedown::instance()->text($this->body);
+        return clean($this->bodyHtml());
     }
 
     public function answers()
@@ -78,5 +78,18 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
+    }
+
+    public function getExcerptAttribute()
+    {
+        return $this->excerpt(250);
+    }
+    public function excerpt($length)
+    {
+        return Str::limit(strip_tags($this->bodyHtml()), $length);
+    }
+    private function bodyHtml()
+    {
+        return \Parsedown::instance()->text($this->body);
     }
 }
