bcd50d8cb7d3d0471307420cfa62e0f42c4d1706
muupa <muupa@supatcharakuns-MacBook-Pro.local>
2020-01-08 10:38:45 +0700

    Rewrite the vote control into vue.js component

--- app/Http/Controllers/VoteAnswerController.php
+++ app/Http/Controllers/VoteAnswerController.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Answer;
 use Illuminate\Http\Request;
-
 class VoteAnswerController extends Controller
 {
     public function __construct()
@@ -14,7 +11,13 @@ class VoteAnswerController extends Controller
     public function __invoke(Answer $answer)
     {
         $vote = (int) request()->vote;
-        auth()->user()->voteAnswer($answer, $vote);
+        $votesCount = auth()->user()->voteAnswer($answer, $vote);
+        if (request()->expectsJson()) {
+            return response()->json([
+                'message' => 'Thanks for the feedback',
+                'votesCount' => $votesCount
+            ]);
+        }
         return back();
     }
 }
