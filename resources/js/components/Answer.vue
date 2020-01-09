398a1c829414fc0f35ae9361dd6a47e80a9a8455
muupa <muupa@supatcharakuns-MacBook-Pro.local>
2020-01-09 15:17:41 +0700

    Create Question and Question Page components

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -29,8 +29,12 @@
     </div>
 </template>
 <script>
+import Vote from './Vote.vue';
+import UserInfo from './UserInfo.vue';
+
 export default {
     props: ['answer'],
+    components: { Vote, UserInfo },
     data () {
         return {
             editing: false,
