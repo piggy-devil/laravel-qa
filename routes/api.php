dbf2c9c95141caed0c051f7cf154d6d691a90858
piggy-devil <muupaza@gmail.com>
2020-01-13 11:35:03 +0700

    Create api endpoints for vote question and answer

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,8 @@ Route::get('/questions/{question}/answers', 'Api\AnswersController@index');
 Route::middleware(['auth:api'])->group(function() {
     Route::apiResource('/questions', 'Api\QuestionsController')->except('index');
     Route::apiResource('/questions.answers', 'Api\AnswersController')->except('index');
+    Route::post('/questions/{question}/vote', 'Api\VoteQuestionController');
+    Route::post('/answers/{answer}/vote', 'Api\VoteAnswerController');
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
