06d6e029705565038d6f98096035c4ddbcad52ab
piggy-devil <muupaza@gmail.com>
2020-01-13 16:26:06 +0700

    fix vulnerable npm packages & update fontawesome

--- resources/js/fontawesome.js
+++ resources/js/fontawesome.js
@@ -1,7 +1,9 @@
-import fontawesome from '@fortawesome/fontawesome';
-import {faCaretUp} from '@fortawesome/free-solid-svg-icons/faCaretUp';
-import {faCaretDown} from '@fortawesome/free-solid-svg-icons/faCaretDown';
-import {faStar} from '@fortawesome/free-solid-svg-icons/faStar';
-import {faCheck} from '@fortawesome/free-solid-svg-icons/faCheck';
+import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
 
-fontawesome.library.add(faCaretUp, faCaretDown, faCheck, faStar);
+config.autoReplaceSvg = 'nest'
+
+import { faCaretUp, faCaretDown, faStar, faCheck } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCaretUp, faCaretDown, faCheck, faStar);
+
+dom.watch()
